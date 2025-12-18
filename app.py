5973997f8653ff082dee3fababa07950741c4f9c
zaman <zamanvardha@gmail>
2025-12-18 09:42:25 +0530

    new3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+import psycopg2
 
 app = Flask(__name__)
 app.secret_key = "super-secret-key" 
