4ab029461758f3bfefe681d7d1bca5bf4ae52156
zaman <zamanvardha@gmail>
2025-12-18 23:23:01 +0530

    already-

--- app.py
+++ app.py
@@ -1,54 +1,42 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import psycopg2
+import os
 
 app = Flask(__name__)
 app.secret_key = "super-secret-key" 
 
-ADMIN_EMAIL = "admin@gmail.com"
-ADMIN_PASSWORD_HASH = generate_password_hash(
-    "admin123",
+USER_EMAIL = "user@gmail.com"
+USER_PASSWORD_HASH = generate_password_hash(
+    "user123",
     method="pbkdf2:sha256",
     salt_length=16
 )
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-
-        if email == ADMIN_EMAIL and check_password_hash(ADMIN_PASSWORD_HASH, password):
-            session["admin_logged_in"] = True
-            session["admin_email"] = email
-            return redirect(url_for("dashboard"))
-
-        return "Invalid Credentials"
-
     return render_template("login.html")
 
 
 @app.route("/dashboard")
 def dashboard():
-    if "admin_logged_in" in session:
-        return f"Welcome Admin ({session['admin_email']})"
+    if "user_logged_in" in session:
+        return f"Welcome User ({session['user_email']})"
     return redirect(url_for("login"))
 
-@app.route("/home.html")
+@app.route("/home")
 def home():
     return render_template("home.html")
-
-@app.route("/about.html")
-def about():
-    return render_template("login.html")
-
-@app.route()
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("login"))
 
+@app.route("/already")
+def already():
+    return render_template("already.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
