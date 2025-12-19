de1383c4d4cf49c786a65acfb966c116c9ef4485
zaman <zamanvardha@gmail>
2025-12-19 10:00:58 +0530

    new5

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-import psycopg2
+import psycopg2 
 import os
 
 app = Flask(__name__)
